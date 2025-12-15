a0c46e6c68bbdade901863f04d706b4931e0f02d
Nikita Starodubtsev <jkee2013@mail.ru>
2025-12-15 20:09:23 +0300

    Remove unused folders and files

--- torch_fidelity_own/doc/sphinx/source/example_code.rst
+++
@@ -1,6 +0,0 @@
-:orphan:
-
-sngan_cifar10.py
-================
-
-.. literalinclude:: ../../../examples/sngan_cifar10.py
--- torch_fidelity_own/tests/aws/aws_enable_swap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-
-sudo fallocate -l 32G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
