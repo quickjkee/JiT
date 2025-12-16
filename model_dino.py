0a951980e4db6e8613243faa9984216b923d1b68
Nikita Starodubtsev <jkee2013@mail.ru>
2025-12-16 17:53:21 +0300

    all train

--- model_dino.py
+++ model_dino.py
@@ -165,6 +165,7 @@ class DinoJiT(nn.Module):
         self.dino_model = dino_model
         self.dino_model.requires_grad_(True)
         self.dino_model.train()
+        self.dino_model.mask_token.requires_grad_(False)
 
         self.hidden_size = self.dino_model.embed_dim
         self.num_classes = num_classes
