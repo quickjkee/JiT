1660076d48e7b2388e8840cb12c88057df4b6a98
Nikita Starodubtsev <jkee2013@mail.ru>
2026-01-12 23:04:00 +0300

    1

--- engine_jit.py
+++ engine_jit.py
@@ -166,7 +166,7 @@ def evaluate(model_without_ddp, args, epoch, batch_size=64, log_writer=None):
 
     # compute FID and IS
     if log_writer is not None:
-        if args.img_size == 256:
+        if args.img_size == 256 or args.img_size == 224:
             fid_statistics_file = 'fid_stats/jit_in256_stats.npz'
         elif args.img_size == 512:
             fid_statistics_file = 'fid_stats/jit_in512_stats.npz'
