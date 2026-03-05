f8387b1e1d5c9f1f05c5c94dbd1b782bad0495b6
Nikita Starodubtsev <jkee2013@mail.ru>
2026-03-05 15:55:37 +0300

    123

--- model_jit.py
+++ model_jit.py
@@ -297,7 +297,7 @@ class DualJiTBlock(nn.Module):
         # Part 2. MLP branch
         x_patch_1 = modulate(self.norm2(x_patch), shift_mlp, scale_mlp)
         x_registers_1 = modulate(self.norm2_context(x_registers), shift_mlp_context, scale_mlp_context)
-        x = self.mlp(torch.cat([x_registers_1, x_patch_1]), dim=1)
+        x = self.mlp(torch.cat([x_registers_1, x_patch_1], dim=1))
 
         x_patch_mlp = x[:, in_context_len:, :]  
         x_registers_mlp = x[:, :in_context_len, :]
