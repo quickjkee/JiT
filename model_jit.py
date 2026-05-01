be35be601b3b071d5c7613076fc4579e36985475
Nikita Starodubtsev <jkee2013@mail.ru>
2026-05-01 18:22:04 +0300

    hz

--- model_jit.py
+++ model_jit.py
@@ -246,10 +246,10 @@ class JiT(nn.Module):
 
         # in-context cls token
         if self.in_context_len > 0:
-            #self.register_tokens = nn.Parameter(torch.zeros(1, self.in_context_len, hidden_size), requires_grad=True)
-            #torch.nn.init.normal_(self.register_tokens, std=.02)
-            self.in_context_posemb = nn.Parameter(torch.zeros(1, self.in_context_len, hidden_size), requires_grad=True)
-            torch.nn.init.normal_(self.in_context_posemb, std=.02)
+            self.register_tokens = nn.Parameter(torch.zeros(1, self.in_context_len, hidden_size), requires_grad=True)
+            torch.nn.init.normal_(self.register_tokens, std=.02)
+            #self.in_context_posemb = nn.Parameter(torch.zeros(1, self.in_context_len, hidden_size), requires_grad=True)
+            #torch.nn.init.normal_(self.in_context_posemb, std=.02)
 
         # rope
         half_head_dim = hidden_size // num_heads // 2
@@ -348,12 +348,12 @@ class JiT(nn.Module):
         for i, block in enumerate(self.blocks):
             # in-context
             if self.in_context_len > 0 and i == self.in_context_start:
-                in_context_tokens = y_emb.unsqueeze(1).repeat(1, self.in_context_len, 1)
-                in_context_tokens += self.in_context_posemb
-                x = torch.cat([in_context_tokens, x], dim=1)
+                #in_context_tokens = y_emb.unsqueeze(1).repeat(1, self.in_context_len, 1)
+                #in_context_tokens += self.in_context_posemb
+                #x = torch.cat([in_context_tokens, x], dim=1)
                 
-                #register_tokens = self.register_tokens.expand(x.shape[0], -1, -1)
-                #x = torch.cat([register_tokens, x], dim=1)
+                register_tokens = self.register_tokens.expand(x.shape[0], -1, -1)
+                x = torch.cat([register_tokens, x], dim=1)
                 
             x = block(x, c, self.feat_rope if i < self.in_context_start else self.feat_rope_incontext)
 
@@ -366,19 +366,19 @@ class JiT(nn.Module):
 
 
 def JiT_B_16(**kwargs):
-    return JiT(depth=12, hidden_size=768, num_heads=12, mlp_ratio=5.27,
+    return JiT(depth=12, hidden_size=768, num_heads=12, #mlp_ratio=5.27,
                bottleneck_dim=128, patch_size=16, **kwargs) # in_context_len=32, in_context_start=4,
 
 def JiT_B_32(**kwargs):
-    return JiT(depth=12, hidden_size=768, num_heads=12, mlp_ratio=5.27,
+    return JiT(depth=12, hidden_size=768, num_heads=12, #mlp_ratio=5.27,
                bottleneck_dim=128, patch_size=32, **kwargs) # in_context_len=32, in_context_start=4,
 
 def JiT_L_16(**kwargs):
-    return JiT(depth=24, hidden_size=1024, num_heads=16, mlp_ratio=5.17,
+    return JiT(depth=24, hidden_size=1024, num_heads=16, #mlp_ratio=5.17,
                bottleneck_dim=128, patch_size=16, **kwargs) # in_context_len=32, in_context_start=8,
 
 def JiT_L_32(**kwargs):
-    return JiT(depth=24, hidden_size=1024, num_heads=16, mlp_ratio=5.17,
+    return JiT(depth=24, hidden_size=1024, num_heads=16, #mlp_ratio=5.17,
                bottleneck_dim=128, patch_size=32, **kwargs) # in_context_len=32, in_context_start=8,
 
 def JiT_H_16(**kwargs):
