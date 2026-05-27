83b700b7e1e434c69fcd3ad9d791403dc32638ef
Nikita Starodubtsev <jkee2013@mail.ru>
2026-05-27 18:08:43 +0300

    cls regs

--- model_jit.py
+++ model_jit.py
@@ -366,57 +366,62 @@ class JiT(nn.Module):
         return imgs
 
     def forward(self, x, t, y, y_cls=None):
-        """
-        x: (N, C, H, W)
-        t: (N,)
-        y: (N,)
-        """
-        # class and time embeddings
         t_emb = self.t_embedder(t)
         y_emb = self.y_embedder(y)
         c = t_emb + y_emb
 
-        # forward JiT
         x = self.x_embedder(x)
         x += self.pos_embed
 
-        # for linear probing
-        if y_cls is not None:
-            y_uncond = self.y_embedder(y_cls)   
-            c_uncond = t_emb + y_uncond                          
+        run_cls_branch = y_cls is not None
+        x_regs_uncond = None
+
+        if run_cls_branch:
+            assert self.cls_depth >= self.in_context_start, \
+                "cls_depth must be >= in_context_start so classifier sees registers"
+            assert self.cls_depth < len(self.blocks), \
+                "cls_depth must be smaller than model depth"
+
+            y_uncond = self.y_embedder(y_cls)
+            c_uncond = t_emb + y_uncond
             x_uncond = x.clone()
-            run_cls_branch = True
 
         for i, block in enumerate(self.blocks):
-            # diffusion block
+            # diffusion branch
             if self.in_context_len > 0 and i == self.in_context_start:
                 in_context_tokens = y_emb.unsqueeze(1).repeat(1, self.in_context_len, 1)
-                in_context_tokens += self.in_context_posemb
+                in_context_tokens = in_context_tokens + self.in_context_posemb
                 x = torch.cat([in_context_tokens, x], dim=1)
-            x = block(x, c, self.feat_rope if i < self.in_context_start else self.feat_rope_incontext)
 
-            # cls block
+            rope = self.feat_rope if i < self.in_context_start else self.feat_rope_incontext
+            x = block(x, c, rope)
+
+            # classifier branch
             if run_cls_branch:
                 if self.in_context_len > 0 and i == self.in_context_start:
                     in_context_tokens = y_uncond.unsqueeze(1).repeat(1, self.in_context_len, 1)
-                    in_context_tokens += self.in_context_posemb
+                    in_context_tokens = in_context_tokens + self.in_context_posemb
                     x_uncond = torch.cat([in_context_tokens, x_uncond], dim=1)
-                x_uncond = block(x_uncond, c_uncond, self.feat_rope if i < self.in_context_start else self.feat_rope_incontext)
+
+                x_uncond = block(x_uncond, c_uncond, rope)
 
                 if i == self.cls_depth:
-                    x_regs_uncond = pool_semantic_registers(x_uncond[:, :self.in_context_len].clone(), exclude_top_frac=0.15)
+                    x_regs_uncond = pool_semantic_registers(
+                        x_uncond[:, :self.in_context_len],
+                        exclude_top_frac=0.15,
+                    )
                     x_regs_uncond = self.classifier(x_regs_uncond)
                     run_cls_branch = False
 
         x = x[:, self.in_context_len:]
-
         x = self.final_layer(x, c)
         output = self.unpatchify(x, self.patch_size)
 
         if y_cls is None:
             return output
-        else:
-            return output, x_regs_uncond
+
+        assert x_regs_uncond is not None, "Classifier branch did not produce logits"
+        return output, x_regs_uncond
 
 
 def JiT_B_16(**kwargs):
